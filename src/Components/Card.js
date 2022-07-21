2a621e60b6fd87b23164a9cbc07277b0b2c59b56
Tombh6 <tombh6@cs.colman.ac.il>
2022-07-21 14:32:06 +0300

    Added Card component

---
+++ src/Components/Card.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from "styled-components";
+import Rating from '@material-ui/lab/Rating';
+
+
+function Card() {
+  return (
+    
+    <Container>
+        <Image> {/*For image links that which will appear on the cards*/}
+        <img src="https://m.media-amazon.com/images/I/51tYxedQ1-L._AC_UF452,452_FMjpg_.jpg" alt="" />
+        </Image>
+        <Description>
+         <h5>SAMSUNG 85" Class QLED 4K UHD Q60B Series Dual LED Quantum HDR, Motion Xcelerator</h5>
+         <Rating name="half-rating-read" defaultValue={4.5} precision={0.5} readOnly />
+         <p>$ 250</p>
+
+         <button>Add to cart</button>
+        </Description>
+    </Container>
+  )
+}
+
+const Container= styled.div`
+width: 100%;
+height: 100%;
+
+display: flex;
+flex-direction: column;
+background-color: #fff;
+z-index: 10;
+ border: 1px solid red; /*Card border color */
+`;
+const Image= styled.div`
+width: 100%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin-top: 20px;
+flex: 0.3;
+
+img{
+    width: 180px;
+    height: 200px;
+}
+`;
+const Description= styled.div`
+width: 90%;
+margin: auto;
+display: flex;
+flex-direction: column;
+justify-content: space-evenly;
+flex: 0.7;
+
+h5{
+    font-size: 16px;
+    font-weight: 600;
+}
+p{
+    font-weight: 600;
+}
+
+button{
+    width: 100%;
+    height: 33px;
+    background-color: #fa8900;
+    border: none;
+    border-radius: 10px;
+
+    cursor: pointer;
+}
+`;
+
+export default Card
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import Navbar from './Navbar'
+import Card from './Card'
+
 
 function Home(){
     return(
@@ -8,21 +10,58 @@ function Home(){
             <Navbar/>
             <Banner>
             <img src="./amazonBanner.jpg" alt="" />
-
+             <img src="./amazonBanner.jpg" alt="" /> {/*For big image in mobile screen */}
             </Banner>
+            <Main>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </Main>
         </Container>
     )
 }
 
 const Container= styled.div`
 width: 100%;
+
+background-color: rgb(234, 237, 237);
 `;
 const Banner= styled.div`
 width: 100%;
 img{
     width: 100%;
     -webkit-mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 2),rgba(0, 0, 0, 0.85),rgba(0, 0, 0, 0.75),rgba(0, 0, 0, 0.55),rgba(0, 0, 0, 0));
+
+    &:nth-child(2){
+        display:none;
+    }
+
+    @media only screen and (max-width:767px){
+        &:nth-child(1){
+            display: none;
+        }
+        &:nth-child(2){
+            display:block;
+            -webkit-mask-image: none;
+        }
+    }
 }
+`;
+
+const Main = styled.div`
+display: grid;
+justify-content: center;
+place-items: center;
+width: 100%;
+
+grid-auto-rows: 420px;
+grid-template-columns: repeat(4,300px);
+grid-gap: 20px;
 `;
 
 export default Home
