f04c94cca316f08d0902bd6b9e052f2abfd828c7
Tombh6 <tombh6@cs.colman.ac.il>
2022-08-24 15:09:12 +0300

    Added Server

---
+++ Shop-Backend/Server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const port= 3001;
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//Connection URL
+const connection_url = "mongodb+srv://Tombh6:0505795582@cluster0.uumqpcw.mongodb.net/Cluster0?retryWrites=true&w=majority"
+
+mongoose.connect(connection_url, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+});
+//API
+app.get("/",(req,res) => res.status(200).send("Home Page"));
+
+app.listen(port, () => console.log("Listening on the port:", port));
